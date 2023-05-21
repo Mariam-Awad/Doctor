bf2ada163fd3ea12a9e7aa0979e3be065b18423b
AhmedTalal <ahmedtala358@gmail.com>
2023-05-21 14:11:16 +0300

    add appAssetUtil screen

---
+++ lib/core/utils/app_assets_util.dart
@@ -0,0 +1,72 @@
+class AppAssetsUtil{
+
+  /// icons section with svg extension >>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>
+  static const String addAltIcon = "assets/icons/add-alt.svg";
+  static const String appleIcon = "assets/icons/apple.svg";
+  static const String attachmentLineIcon = "assets/icons/attachment-line.svg";
+  static const String bookIcon = "assets/icons/book.svg";
+  static const String calendarIcon = "assets/icons/calendar.svg";
+  static const String calendarIcon2 = "assets/icons/calendar3.svg";
+  static const String cancelCircleIcon = "assets/icons/cancel-circle.svg";
+  static const String cardLogoIcon = "assets/icons/card logo.svg";
+  static const String chatIcon = "assets/icons/chat.svg";
+  static const String checkCircleFillIcon = "assets/icons/check-circle-fill.svg";
+  static const String clockIcon = "assets/icons/clock.svg";
+  static const String facebookIcon = "assets/icons/facebook.svg";
+  static const String femaleIcon = "assets/icons/female.svg";
+  static const String group1Icon = "assets/icons/Group 35902.svg";
+  static const String group2Icon = "assets/icons/Group 35952.svg";
+  static const String group3Icon = "assets/icons/Group 35962.svg";
+  static const String group4Icon = "assets/icons/Group 36039.svg";
+  static const String group5Icon = "assets/icons/Group 36040.svg";
+  static const String group6Icon = "assets/icons/Group 36041.svg";
+  static const String group7Icon = "assets/icons/Group 36195.svg";
+  static const String group8Icon = "assets/icons/Group 36196.svg";
+  static const String group9Icon = "assets/icons/Group 36197.svg";
+  static const String group10Icon = "assets/icons/Group 36198.svg";
+  static const String group11Icon = "assets/icons/Group 36060.svg";
+  static const String homeIcon = "assets/icons/home.svg";
+  static const String chatSenderIcon = "assets/icons/Icon.svg";
+  static const String locationIcon = "assets/icons/Icon material-location-on.svg";
+  static const String maleIcon = "assets/icons/male.svg";
+  static const String googleIcon = "assets/icons/Mask Group 20.svg";
+  static const String doctorIcon = "assets/icons/my doctor.svg";
+  static const String notificationIcon = "assets/icons/not.svg";
+  static const String mapsPathIcon = "assets/icons/Path 33363.svg";
+  static const String scanIcon = "assets/icons/scan.svg";
+  static const String wifiIcon = "assets/icons/wifi.svg";
+
+
+  /// images section with png or jpg or jpeg extension >>>>>>>>>>>>>>>>>>>>>>>>>>>>>.
+  static const String patientBackgroundSama3aImage = "assets/images/Add Patient Details -Personal Information 1 – 2.jpg";
+  static const String patientBackgroundSheretImage = "assets/images/Add Patient Details – 8.jpg";
+  static const String patientBackgroundPlusImage = "assets/images/Add Patient Details – 9.jpg";
+  static const String patientBackgroundEmptyImage = "assets/images/Add Patient Details – 9 copy.jpg";
+  static const String patientBackgroundAnabebImage = "assets/images/Add Patient Details – 10.jpg";
+  static const String artboaImage = "assets/images/Artboa.png";
+  static const String artBoardImage = "assets/images/Artboard – 1.png";
+  static const String patientBackgroundAttachmentImage = "assets/images/Attachments.jpg";
+  static const String patientBackgroundCheckOutImage = "assets/images/Checkout.jpg";
+  static const String patientBackgroundAppointmentImage = "assets/images/Choose appointment date and time.jpg";
+  static const String patientBackgroundLocationImage = "assets/images/Choose Branch.jpg";
+  static const String AnabebImage = "assets/images/Coronavirus Blood Samples Mixed.G03.2k.png";
+  static const String patientBackgroundTermometerImage = "assets/images/Define Problem- Treatment – 1.jpg";
+  static const String termometerImage = "assets/images/Digital Thermometer.B04.2k.png";
+  static const String doctorImage = "assets/images/doctor.jpg";
+  static const String homeBackground1Image = "assets/images/Home -1.jpg";
+  static const String homeBackground2Image = "assets/images/Home – 2.jpg";
+  static const String homeBackground3Image = "assets/images/Home – 4.jpg";
+  static const String homeBackground4Image = "assets/images/Home – 5.jpg";
+  static const String homeBackground5Image = "assets/images/Home – 6.jpg";
+  static const String loginBackgroundImage = "assets/images/Login.jpg";
+  static const String capsolaImage1 = "assets/images/Mask Group 14.png";
+  static const String sama3aImage = "assets/images/Mask Group 15.png";
+  static const String paymentBackgroundImage = "assets/images/Payment.jpg";
+  static const String reviewBackgroundImage = "assets/images/Review.jpg";
+  static const String profileBackgroundImage = "assets/images/User Profile – 4.jpg";
+  static const String capsolaBackgroundImage = "assets/images/Verify Identity.jpg";
+  static const String weightBackgroundImage = "assets/images/What is your weight.jpg";
+  static const String plusImage = "assets/images/White Round Medical Cross Symbol.H03.2k.png";
+  static const String cotnainerBackgroundImage = "assets/images/widget.png";
+
+}
