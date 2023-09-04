33c62e42c4612ce4290b8319d99da62b06989592
AhmedTalal <ahmedtala358@gmail.com>
2023-09-04 20:01:20 +0300

    some changes

--- lib/presentation/bloc/app_generate_bloc/app_generate_states.dart
+++ lib/presentation/bloc/app_generate_bloc/app_generate_states.dart
@@ -1,23 +1,23 @@
 import 'package:doctor/domain/entities/app_entity.dart';
 
-abstract class AppGenerateStates{
+abstract class AppGenerateStates {
   const AppGenerateStates();
 }
 
-class AppGenerateInitState extends AppGenerateStates{
+class AppGenerateInitState extends AppGenerateStates {
   const AppGenerateInitState();
 }
 
-class GetAppModelLoadingState extends AppGenerateStates{
+class GetAppModelLoadingState extends AppGenerateStates {
   //TODO:implement state here
 }
 
-class GetAppModelLoadedState extends AppGenerateStates{
+class GetAppModelLoadedState extends AppGenerateStates {
   final AppEntity appEntity;
   const GetAppModelLoadedState({required this.appEntity});
 }
 
-class GetAppModelFailedState extends AppGenerateStates{
+class GetAppModelFailedState extends AppGenerateStates {
   //TODO:implement failed state here
   const GetAppModelFailedState();
 }
